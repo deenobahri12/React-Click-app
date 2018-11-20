6ac180ea8c29edf330f3e2fb95b9fd70a284961f
deeno bahri <deenobahri12@gmail.com>
2018-11-20 00:14:08 -0600

    Updates

---
+++ precache-manifest.a0cdd123009f004deb49a7abc1fd6f6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c453c050556ff36266ad",
+    "url": "/React-Click-app/static/js/runtime~main.c453c050.js"
+  },
+  {
+    "revision": "6b238e2cb007ebbde315",
+    "url": "/React-Click-app/static/js/main.6b238e2c.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/React-Click-app/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "6b238e2cb007ebbde315",
+    "url": "/React-Click-app/static/css/main.f8fc379e.chunk.css"
+  },
+  {
+    "revision": "c31e33ccc89fe9671f93c6d547bc4432",
+    "url": "/React-Click-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.cb343b9983eadf7411d7da65e53b66ee.js"
+  "/React-Click-app/precache-manifest.a0cdd123009f004deb49a7abc1fd6f6a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/React-Click-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
